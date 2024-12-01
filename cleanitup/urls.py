8ef452f42577b48ac753aae7505bb4766c0b5d6f
AlexanderPavon <152736860+AlexanderPavon@users.noreply.github.com>
2024-12-01 17:19:37 -0500

    Update urls.py

--- cleanitup/urls.py
+++ cleanitup/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
 from . import views
 
 urlpatterns = [
@@ -7,4 +8,9 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('game/', views.game_view, name='game'),
+
+    path('aboutus/', views.about_us, name='aboutus'),
+    path('ranking/', views.ranking, name='ranking'),
+    path('plantations/', views.plantations, name='plantations'),
+    path('knowmore/', views.know_more, name='knowmore'),
 ]
